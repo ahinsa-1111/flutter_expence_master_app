2dcf7ea06754ee313e93f3adbbbd2abe245cebe7
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-01-23 12:14:52 +0530

    commit_05

--- lib/pages/expences.dart
+++ lib/pages/expences.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expence_master_app/models/expence.dart';
 
 class Expences extends StatefulWidget {
   const Expences({super.key});
@@ -8,26 +9,47 @@ class Expences extends StatefulWidget {
 }
 
 class _ExpencesState extends State<Expences> {
+  //expenceList
+  final List<ExpenceModel> _expenceList = [
+    ExpenceModel(
+      title: "Football",
+      amount: 100,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    ExpenceModel(
+        title: "Carrot",
+        amount: 2500.00,
+        date: DateTime.now(),
+        category: Category.food)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title:
-          const Text("Expence Master", style: TextStyle(color: Colors.white)),
-      backgroundColor: const Color.fromARGB(255, 121, 35, 61),
-      elevation: 1000,
-      actions: [
-        Container(
-          color: const Color.fromARGB(255, 44, 28, 33),
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.add,
-              color: Colors.white,
+          title: const Text("Expence Master",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromARGB(255, 121, 35, 61),
+          elevation: 1000,
+          actions: [
+            Container(
+              color: const Color.fromARGB(255, 44, 28, 33),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      ],
-    ));
+        body: ListView.builder(
+          itemCount: _expenceList.length,
+          itemBuilder: (context, index) {
+            return Text(_expenceList[index].title);
+          },
+        ));
   }
 }
