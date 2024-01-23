2635d3ccf2b27486cd723185c2b52a3442d25f1e
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-01-23 11:43:19 +0530

    commit_02

---
+++ lib/pages/expences.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
